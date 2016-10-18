ea93342a6ae9d593871c124eab63792b834a70aa
SinedVonotirah <sined.vonotirah@gmail.com>
2016-10-18 15:10:01 +0300

    simple fix in App.java

--- src/main/java/by/vonotirah/classLoaderTask/App.java
+++ src/main/java/by/vonotirah/classLoaderTask/App.java
@@ -4,10 +4,11 @@ import by.vonotirah.classLoaderTask.implementing.JarClassLoader;
 import by.vonotirah.classLoaderTask.implementing.MenuEngineImpl;
 import by.vonotirah.classLoaderTask.interfaces.CustomClassLoader;
 import by.vonotirah.classLoaderTask.interfaces.LClass;
+import by.vonotirah.classLoaderTask.interfaces.MenuEngine;
 
 public class App {
 	public static void main(String[] args) throws Exception {
-		MenuEngineImpl menu = new MenuEngineImpl(
+		MenuEngine menu = new MenuEngineImpl(
 				(CustomClassLoader) new JarClassLoader());
 		LClass loadedClass = (LClass) menu.startEngine().newInstance();
 		loadedClass.run();
